b2eb6b90cb79c21eb5f7cde036e14a93af22abfa
Hugo Sá <hugo.sousa.sa@hotmail.com>
2021-11-19 17:46:50 +0000

    Adding p2

---
+++ p2/src/JumpBubbleSort.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class JumpBubbleSort {
+
+    /*
+    0 2 1 3 6 4 5 8 7 9 -> maior elemento passa para topo
+    9 0 2 1 3 6 4 5 8 7 -> corta o primeiro elemento
+    0 2 1 3 6 4 5 8 7 -> maior elemento passa para o topo
+    8 0 2 1 3 6 4 5 7 -> corta o primeiro elemento
+    0 2 1 3 6 4 5 7 -> maior elemento passa para topo
+    7 0 2 1 3 6 4 5 -> corta o primeiro elemento
+    0 2 1 3 6 4 5 -> repetir ate ultimo elemento ser cortado
+    resutltado final = {9 8 7 6 5 4 3 2 1 0} 
+    */
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[];
+        while()
+        System.out.println("merda");
+
+    }
+}
