5b553e8b96450d2e6540fe5b88e0de98e9e66649
Hugo Sá <hugo.sousa.sa@hotmail.com>
2021-11-25 13:02:39 +0000

    Introducing bubblesort

--- p2/src/JumpBubbleSort.java
+++ p2/src/JumpBubbleSort.java
@@ -13,28 +13,52 @@ public class JumpBubbleSort {
     resutltado final = {9 8 7 6 5 4 3 2 1 0} 
     */
 
-    
+    public static void exchange(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
 
-    public static void sort(int[] array) {
+    public static int[] simpleSort(int[] array) {
         int temp = 0;
-        int dist = array.length-1;
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] < array[i-1]) {
-                temp = array[i-1];
-                array[i-1] = array[i];
-                array[i] = temp;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i+1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
+                }
             }
+            
         }
+        return array;
+    }
+
+    public static int[] bubbleSort(int[] array) {
+        int dist = array.length-1;
+        int temp = 0;
+        for (int i = 0; i < array.length; i++) {
+         if (array[i] > array[dist]) {
+             temp = array[dist];
+             array[dist] = array[i];
+         }
+        }
+
+
+
+        return array;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        //Scanner scanner = new Scanner(System.in);
         int[] array = {6,5,9,2,4,7,1,8,3};
-        int indice = 0;
+        //int indice = 0;
         // while (scanner.hasNext()) {
         //     array[indice] = scanner.nextInt();
         //     indice++;
         // }
-        sort(array);
+        simpleSort(array);
+        System.out.println(Arrays.toString(array));
+        //scanner.close();
     }
 }
