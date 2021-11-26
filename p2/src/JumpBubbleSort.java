dd0946a33b98ed75307f9fadd2bd26e1fb860991
Hugo Sá <56072206+SurfZxx@users.noreply.github.com>
2021-11-26 04:31:43 +0000

    Algorithm done

--- p2/src/JumpBubbleSort.java
+++ p2/src/JumpBubbleSort.java
@@ -1,6 +1,7 @@
 import java.util.*;
+//package aed.sorting;
 
-public class JumpBubbleSort {
+public class JumpBubbleSort{
 
     /*
     0 2 1 3 6 4 5 8 7 9 -> maior elemento passa para topo
@@ -13,51 +14,59 @@ public class JumpBubbleSort {
     resutltado final = {9 8 7 6 5 4 3 2 1 0} 
     */
 
+
+
+//    public static int[] simpleSort(int[] array) {
+//        int temp = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = i+1; j < array.length; j++) {
+//                if (array[i] > array[j]) {
+//                    temp = array[j];
+//                    array[j] = array[i];
+//                    array[i] = temp;
+//                }
+//            }
+//
+//        }
+//        return array;
+//    }
+
+    public static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
     public static void exchange(Comparable[] a, int i, int j) {
         Comparable t = a[i];
         a[i] = a[j];
         a[j] = t;
     }
 
-    public static int[] simpleSort(int[] array) {
-        int temp = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i+1; j < array.length; j++) {
-                if (array[i] > array[j]) {
-                    temp = array[j];
-                    array[j] = array[i];
-                    array[i] = temp;
+    public static <T extends  Comparable<T>> void sort(T[] array) {
+        int n = array.length;
+        int d=n-1;
+        boolean swapped = false;
+        while (d>=1)   {
+            swapped = false ;
+            for (int j = d; j < n; j++ )  {
+                if  (less(array[j],array[j-d]))   {
+                    exchange(array, j,j-d);
+                    swapped = true;
                 }
             }
-            
+            if(!swapped) return;
+            d= (int) Math.floor(d*0.77);
         }
-        return array;
-    }
-
-    public static int[] bubbleSort(int[] array) {
-        int dist = array.length-1;
-        int temp = 0;
-        for (int i = 0; i < array.length; i++) {
-         if (array[i] > array[dist]) {
-             temp = array[dist];
-             array[dist] = array[i];
-         }
-        }
-
-
-
-        return array;
     }
 
     public static void main(String[] args) {
         //Scanner scanner = new Scanner(System.in);
-        int[] array = {6,5,9,2,4,7,1,8,3};
+        Integer[] array = {6,5,9,2,4,7,1,8,3};
         //int indice = 0;
         // while (scanner.hasNext()) {
         //     array[indice] = scanner.nextInt();
         //     indice++;
         // }
-        simpleSort(array);
+        sort(array);
         System.out.println(Arrays.toString(array));
         //scanner.close();
     }
