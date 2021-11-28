86ccfecc2c9b7c1cf0d7676030aabc98767a576a
Hugo Sá <56072206+SurfZxx@users.noreply.github.com>
2021-11-28 19:55:31 +0000

    Update QuickSort3Way.java

--- p2/src/QuickSort3Way.java
+++ p2/src/QuickSort3Way.java
@@ -3,6 +3,16 @@
 import java.util.*;
 
 
-public class QuickSort3Way {
+public class QuickSort3Way extends Sort {
+
+    public static <T extends Comparable<T>> void sort(T[] array) {
+        int lt = 0;
+        int gt = 0;
+        int pivo = 0;
+        
+    }
     
+    public static void main(String[] args) {
+    	
+    }
 }
