6197566722e6e5177e199921681702c6ac2f1210
Hugo Sá <56072206+SurfZxx@users.noreply.github.com>
2021-11-28 20:12:41 +0000

    Update QuickSort3Way.java

--- p2/src/QuickSort3Way.java
+++ p2/src/QuickSort3Way.java
@@ -6,10 +6,16 @@ import java.util.*;
 public class QuickSort3Way extends Sort {
 
     public static <T extends Comparable<T>> void sort(T[] array) {
-        int lt = 0;
-        int gt = 0;
-        int pivo = 0;
-        
+        T lt = array[0];
+        T gt = array[array.length];
+        T pivo = array[0];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < array.length; j++) {
+                if (array[j] < pivo) {
+                    
+                }
+            }
+        }
     }
     
     public static void main(String[] args) {
