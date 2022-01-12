21919740535fbc0bd0e3e772a44e905884fc5979
Hugo Sá <56072206+SurfZxx@users.noreply.github.com>
2022-01-12 22:47:28 +0000

    Created P4 project

---
+++ p4/src/MaxCycleMST.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class MaxCycleMST {
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello there");
+        scanner.close();
+    }
+}
