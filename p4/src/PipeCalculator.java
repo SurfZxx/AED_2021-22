ee95258f2eb350b16955e0ec7da2297e6681f976
Hugo Sá <56072206+SurfZxx@users.noreply.github.com>
2022-01-21 22:56:43 +0000

    Create PipeCalculator.java

---
+++ p4/src/PipeCalculator.java
@@ -0,0 +1,31 @@
+//package aed.graphs;
+
+import java.util.Stack;
+
+    public class PipeCalculator {
+
+        public PipeCalculator(int n, float[] well, float[][] costs) {
+
+        }
+
+        public UndirectedWeightedGraph createGraph(int n, float[] well, float[][] costs) {
+
+        }
+
+        public UndirectedWeightedGraph calculateSolution(UndirectedWeightedGraph g) {
+
+        }
+
+        public UndirectedWeightedGraph calculateSolution() {
+
+        }
+
+        public UndirectedWeightedGraph getMST() {
+            return mst;
+        }
+
+
+        public static void main(String[] args) {
+
+        }
+    }
