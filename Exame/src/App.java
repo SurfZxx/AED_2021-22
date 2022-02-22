da9f35df56264a5dd7ef833ee02bfc8fd9c68547
Hugo Sá <56072206+SurfZxx@users.noreply.github.com>
2022-02-22 22:16:49 +0000

    Final

--- Exame/src/App.java
+++ Exame/src/App.java
@@ -1,20 +1,53 @@
-public class App {
+import java.util.*;
+
+public class App<StackList, QueueList> {
 
 
-    public static int xpto2(int[] array, int n) {
+
+    // public static void exer() {
+    //     StackList<Integer> s = new StackList<Integer>();
+    //     QueueList<Integer> q = new QueueList<Integer>();
+    //     for(int i = 1; i< 7; i++) {
+    //         s.push(i);
+    //         q.enqueue(i);
+    //     }
+    //     while(!q.isEmpty()) s.push(q.dequeue());
+    //     while(!s.isEmpty())  {
+    //         System.out.print(s.pop() + ",");
+    //     }
+    // }
+
+
+    // public static int xpto2(int[] array, int n) {
+    //     int result = 0;
+    //     for (int i = 0; i < array.length; i++) {
+    //         if (array[i]%n == 0) {
+    //             result = result + array[i];
+    //         }
+    //     }
+    //     return result;
+    // }
+
+    public static int calcNPares(int n, int[] array) {
         int result = 0;
+        int count = 0;
         for (int i = 0; i < array.length; i++) {
-            if (array[i]%n == 0) {
-                result = result + array[i];
+            if (count <= n-1 && array[i]%2 == 0) {
+                    result = result + array[i];
+                    count++;
             }
         }
         return result;
     }
 
     public static void main(String[] args) {
-        int[] array = {2,4,6,3,5,9};
-        int n = 3;
-        int result = xpto2(array, n);
+//         int[] array = {2,4,6,3,5,9};
+//         int n = 3;
+//         int result = xpto2(array, n);
+//         System.out.println(result);
+        // exer();
+        int[] array = {8,6,5,4,3,2,1};
+        int result = calcNPares(3, array);
         System.out.println(result);
     }
 }
